6abb19e576a0a4e70538aa72d4d5bd4e12587f46
ge-king <george.pking@gmail.com>
2020-10-08 20:32:19 +0100

    Add files via upload

---
+++ diceroll.py
@@ -0,0 +1,36 @@
+import random
+import os
+import discord
+
+# create discord client
+client = discord.Client()
+
+token = 'NzYzODI4MzkzNjY5MTY1MDY2.X39YoA.bmWJWLVeYQ2HUNo64bfEnwwZ80o'
+
+# bot is ready
+@client.event
+async def on_ready():
+	try:
+		# print bot information
+		print(client.user.name)
+		print(client.user.id)
+		print('Discord.py Version: {}'.format(discord.__version__))
+	
+	except Exception as e:
+		print(e)
+
+@client.event
+async def on_message(message):
+	if message.content.startswith('!roll'):
+		randroll = 0
+		rolltotal = 0
+		x,inputroll = message.content.split( )
+		num,sides = inputroll.split("d")
+		await message.channel.send('Rolling '+ num + ' ' + sides + ' sided dice for you.')
+		for x in range (0,int(num)):
+			randroll = random.randint(1, int(sides))
+			await message.channel.send(randroll)
+			rolltotal += randroll
+		await message.channel.send('The total value of those rolls is ' + str(rolltotal) +'.')
+
+client.run(token)
